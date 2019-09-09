ce1b015aae6551f5e8f887f2ecf054f600ed5848
Luca Spiller <luca@stackednotion.com>
2019-09-09 22:18:18 +0300

    Ignore redundant messages from OSRAM GU10 tunable white

--- devices.js
+++ devices.js
@@ -1430,7 +1430,11 @@ const devices = [
         description: 'LIGHTIFY LED PAR16 50 GU10 tunable white',
         supports: generic.light_onoff_brightness_colortemp.supports,
         toZigbee: generic.light_onoff_brightness_colortemp.toZigbee.concat([tz.osram_cmds]),
-        fromZigbee: generic.light_onoff_brightness_colortemp.fromZigbee,
+        fromZigbee: generic.light_onoff_brightness_colortemp.fromZigbee.concat([
+            fz.ignore_onoff_change,
+            fz.ignore_light_brightness_report,
+            fz.ignore_light_color_colortemp_report
+        ]),
     },
     {
         zigbeeModel: ['Classic B40 TW - LIGHTIFY'],
